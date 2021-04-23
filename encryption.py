54c1422a3181b7e94f63da06dbae97af6b864709
joecatt717 <joe_catterson@hotmail.com>
2021-04-23 12:58:39 -0500

    Initial commit

---
+++ encryption.py
@@ -0,0 +1,12 @@
+'''
+Goals:
+1 - Take a simple message and encrypt it using a variety of encryption methods
+    Reverse Cipher
+    Ceasar + Reverse Cipher
+    Substitution
+    Caesar Shift
+2 - Implement PyCrypto, M2Crypto, PyOpenSSL to simplify this cryptography
+    use this to encrypt entire files
+'''
+
+print("Hello, World!")
